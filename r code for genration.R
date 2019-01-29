d390755d0f63a8ec40d916f0b6d3c0a8274613c0
Samridh Sharma <44182152+friday7@users.noreply.github.com>
2019-01-29 13:18:35 +0000

    Add files via upload

---
+++ r code for genration.R
@@ -0,0 +1,32 @@
+library(htmltab) 
+library(stringr) 
+library(tidyr) 
+library(rvest) 
+evt_lst <- read.csv(file="C:\\Users\\sam pc\\Desktop\\tedtalks dwbi\\tedxeventslat.csv", header = TRUE, sep = ",",  stringsAsFactors=FALSE) 
+event_url<- evt_lst[,5] 
+count <- length(evt_lst$urls) 
+location<- matrix(,nrows = count) 
+location
+for (i in 1:count){ 
+url <- evt_lst$urls[2] 
+print(url) 
+Location_lp <- htmltab(doc=url, which = 1)
+Location_lp
+#Location_lp <- toString(Location_lp) 
+location[i,1] <- Location_lp$Location[2] 
+} 
+location <- gsub("Location","",location) 
+location <- gsub('([[:upper:]])', ' \\1', location) 
+write.csv(location, "DimLocations.csv", row.names = FALSE) 
+
+location <- cbind(location,location) #check class of location
+library(maps) 
+data("world.cities") 
+b <- gsub(toString(location), "[[:punct:]]", "") 
+a <- strsplit(location, " ") 
+
+
+c <- lapply(a, function(x)x[max(which(x %in% world.cities$name))]) 
+d <- lapply(a,function(x)x[which(x %in% world.cities$country.etc)]) 
+location$V2 <- NULL 
+write.csv(location, "DimLocations.csv", row.names = FALSE) 
